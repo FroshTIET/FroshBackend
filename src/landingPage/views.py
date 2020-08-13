6cfb3ce7144a4b1f9963afdde84bdf2d9e33086b
Vividh Mariy <vmariya_be19@thapar.edu>
2020-08-14 03:27:19 +0530

    Fixed duplicate object creation

--- src/landingPage/views.py
+++ src/landingPage/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from landingPage.models import ContactFormEntry
 
 def homeView(request):
-    name = request.POST.get("name", "")
-    subject = request.POST.get("subject", "")
-    message = request.POST.get("message", "")
-    email = request.POST.get("email", "")
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        subject = request.POST.get("subject", "")
+        message = request.POST.get("message", "")
+        email = request.POST.get("email", "")
 
-    ContactFormEntry.objects.create(name=name, subject=subject, message=message, email=email)
+        ContactFormEntry.objects.create(name=name, subject=subject, message=message, email=email)
     
     return render(request, "landingPage/index.html", {})
 
