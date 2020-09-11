e79d06df47a19014062517163b08fb61a29351e9
Naman Monga <42845908+Naman-Monga@users.noreply.github.com>
2020-09-11 14:33:07 +0530

    doaa-dosa

--- src/landingPage/urls.py
+++ src/landingPage/urls.py
@@ -3,6 +3,6 @@ from landingPage.views import homeView, dosa_page, director_page
 
 urlpatterns = [
     path("", homeView, name="home-page"),
-    path("dean-letter",dosa_page, name="dosa-page" ),
-    path("director-letter", director_page, name="director-page"),
+    path("dean-letter/",dosa_page, name="dosa-page" ),
+    path("director-letter/", director_page, name="director-page"),
 ]
